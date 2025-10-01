c399a4b8cfa3108ec1a9e8f6ecac9d6355fad3e6
zeeshanwdev27 <zeeshanwdev27@gmail.com>
2025-10-01 16:19:44 +0500

    Added AddCart Overlay & Tooltip for MainNavbar.jsx

---
+++ src/components/Navbar/Main_Navbar/AddToCartOverlay/AddToCartOverlay.jsx
@@ -0,0 +1,138 @@
+import React, { useState } from "react";
+import { X, ShoppingBag, Heart } from "lucide-react";
+import { motion, AnimatePresence } from "motion/react";
+import { Button } from "@/components/ui/button";
+
+function AddToCartOverlay({ addToCart, setAddToCart }) {
+  const [iconActive, setIconActive] = useState(true);
+
+  return (
+    <AnimatePresence>
+      {addToCart && (
+        <div className="fixed inset-0 z-[9999]">
+          {/* Blur background */}
+          <motion.div
+            className="absolute inset-0 backdrop-blur-xs bg-black/75"
+            onClick={() => setAddToCart(false)}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.2 }}
+          />
+
+          {/* Right side sliding panel */}
+          <motion.div
+            className="absolute right-0 top-0 h-full w-[500px] bg-white shadow-lg flex flex-col px-7 py-7"
+            initial={{ x: "100%" }} // start off-screen (to the right)
+            animate={{ x: 0 }} // slide into place
+            exit={{ x: "100%" }} // slide out to right
+            transition={{ duration: 0.4, ease: "easeInOut" }}
+          >
+            {/* Header */}
+            <div className="flex justify-between w-full items-center">
+              <h2 className="text-md font-bold truncate">
+                {iconActive ? "YOUR BAG" : "WISHLIST"}
+              </h2>
+
+              <div className="flex items-center gap-4">
+                <div className="flex justify-center items-center  bg-gray-100 rounded-full cursor-pointer">
+                  <div
+                    onClick={() => setIconActive(!iconActive)}
+                    className={`${
+                      iconActive
+                        ? "bg-black text-white"
+                        : "text-black bg-gray-100 "
+                    } px-5 py-2 rounded-full`}
+                  >
+                    <ShoppingBag className="w-4.5 h-5" strokeWidth={1.5} />
+                  </div>
+
+                  <div
+                    onClick={() => setIconActive(!iconActive)}
+                    className={`${
+                      iconActive
+                        ? "text-black bg-gray-100 "
+                        : "bg-black text-white"
+                    } px-5 py-2 rounded-full`}
+                  >
+                    <Heart className="w-4.5 h-5" strokeWidth={1.5} />
+                  </div>
+                </div>
+
+                <button
+                  onClick={() => setAddToCart(false)}
+                  className="cursor-pointer"
+                >
+                  <X className="w-7 h-7  text-black" strokeWidth={1.4} />
+                </button>
+              </div>
+            </div>
+
+            {/* Body */}
+
+            {/* AddToCart */}
+            {iconActive && (
+              <div className="min-h-screen flex flex-col justify-center gap-5 items-center w-full pb-30">
+                <img
+                  src="https://res.cloudinary.com/dzhovcqy3/image/upload/f_png/v1759315915/empty-bag_womwcc"
+                  alt="empty-trash"
+                />
+
+                <div className="flex flex-col gap-2 w-full justify-center items-center">
+                  <h2 className="text-lg font-bold truncate">
+                    YOUR BAG IS EMPTY
+                  </h2>
+                  <p className="text-sm text-gray-700 truncate">
+                    There are no products in your bag
+                  </p>
+                </div>
+
+                <div className="flex flex-col gap-2.5 w-full justify-center items-center">
+                  <Button className="h-auto w-1/2 py-3 text-sm font-bold cursor-pointer rounded-4xl">
+                    SHOP MENS
+                  </Button>
+                  <Button className="h-auto w-1/2 py-3 text-sm font-bold cursor-pointer rounded-4xl">
+                    SHOP WOMENS
+                  </Button>
+                </div>
+              </div>
+            )}
+
+            {/* WISHLIST */}
+            <div className="min-h-screen flex flex-col justify-center gap-7 items-center w-full pb-30">
+              <div className="flex flex-col gap-3 justify-center items-center">
+                <div className="flex gap-3 items-center">
+                  <Heart className="w-4.5 h-5" strokeWidth={1.5} />
+                  <h2 className="text-lg font-bold truncate">
+                    SAVE TO WISHLIST
+                  </h2>
+                </div>
+
+                <p className="text-[16px] text-black text-balance text-center px-10">
+                  Ever wish you could save all your fave fits & accessories in
+                  one place to come back to later? Almost like a ✨ wishlist ✨.
+                </p>
+              </div>
+
+              <div className="flex flex-col gap-2.5 w-full justify-center items-center">
+                <Button className="h-auto w-1/2 py-3 text-sm font-bold cursor-pointer rounded-4xl">
+                  CREATE ACCOUNT
+                </Button>
+                <Button
+                  className="bg-gray-300 hover:bg-gray-300 hover:opacity-80 text-black h-auto w-1/2 py-3 text-sm font-bold cursor-pointer rounded-4xl"
+                >
+                  LOGIN
+                </Button>
+              </div>
+            </div>
+
+
+
+          </motion.div>
+        </div>
+      )}
+    </AnimatePresence>
+  );
+}
+
+export default AddToCartOverlay;
--- src/components/Navbar/Main_Navbar/Main_Navbar.jsx
+++ src/components/Navbar/Main_Navbar/Main_Navbar.jsx
@@ -10,12 +10,19 @@ import logo from "../../../assets/Gymshark/logo.avif";
 import Menu from "./Menu/Menu";
 import { motion } from "motion/react";
 import SearchBoxOverlay from "./SearchBoxOverlay/SearchBoxOverlay";
-import HamburgerOverlay from "./HamburgerOverlay/HamburgerOverlay"
+import HamburgerOverlay from "./HamburgerOverlay/HamburgerOverlay";
+import AddToCartOverlay from "./AddToCartOverlay/AddToCartOverlay";
 import { Link } from "react-router-dom";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
-function Main_Navbar({isScrolled}) {
+function Main_Navbar({ isScrolled }) {
   const [showSearch, setShowSearch] = useState(false);
-  const [showMenu, setShowMenu] = useState(false)
+  const [showMenu, setShowMenu] = useState(false);
+  const [addToCart, setAddToCart] = useState(false);
 
   // Scroll Hide
   useEffect(() => {
@@ -34,14 +41,36 @@ function Main_Navbar({isScrolled}) {
     }
   }, [showMenu]);
 
+  useEffect(() => {
+    if (addToCart) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [addToCart]);
+
+  // Timer For ToolTip Popup
+  const [tooltipOpen, setTooltipOpen] = useState(false);
+  useEffect(() => {
+    let openTimer,closeTimer;
+      openTimer = setTimeout(() => {
+        setTooltipOpen(true);
+      closeTimer = setTimeout(() => {
+        setTooltipOpen(false);
+      }, 3500); 
+      }, 3000); 
+
+  return () => {
+    clearTimeout(openTimer);
+    clearTimeout(closeTimer);
+  };
+  }, []);
+
   return (
     <div className="bg-white shadow w-full">
-
       {/* Top Row */}
       <div className="flex justify-between items-center py-7 px-7 lg:py-4 lg:px-12 ">
-
         <div className="flex items-center">
-
           {/* Desktop Menu */}
           <div className="hidden lg:block">
             <Menu />
@@ -49,16 +78,21 @@ function Main_Navbar({isScrolled}) {
 
           {/* Mobile Hamburger */}
           <button className="block lg:hidden">
-            <MenuIcon className="w-6 h-6" strokeWidth={1.5} onClick={()=> setShowMenu(true)} />
+            <MenuIcon
+              className="w-6 h-6"
+              strokeWidth={1.5}
+              onClick={() => setShowMenu(true)}
+            />
           </button>
         </div>
 
         {/* Logo */}
         <Link to="/">
-        <img src={logo}
-          className="h-5 pl-10 md:pl-23 lg:pl-45 lg:pr-0 object-contain cursor-pointer"
-          alt="GYMSHARK_LOGO"
-        />
+          <img
+            src={logo}
+            className="h-5 pl-10 md:pl-23 lg:pl-45 lg:pr-0 object-contain cursor-pointer"
+            alt="GYMSHARK_LOGO"
+          />
         </Link>
 
         {/* Right Icons */}
@@ -76,15 +110,53 @@ function Main_Navbar({isScrolled}) {
             </div>
           </li>
 
-          {/* Icons */}
+          {/* Heart Icons */}
           <li className="hidden md:block cursor-pointer hover-lift">
             <Heart className="w-5 h-6 cursor-pointer" strokeWidth={1.5} />
           </li>
-          <li className="cursor-pointer hover-lift">
-            <User className="w-5 h-6 cursor-pointer" strokeWidth={1.5} />
+
+          {/* User Icons */}
+          <li className="cursor-pointer">
+            <Tooltip
+              open={addToCart ? false : tooltipOpen}
+              onOpenChange={() => {}} // prevent reopening on hover
+            >
+              <TooltipTrigger asChild>
+                <User
+                  className="w-5 h-6 cursor-pointer hover-lift"
+                  strokeWidth={1.5}
+                />
+              </TooltipTrigger>
+
+              <TooltipContent
+                side="bottom"
+                align="end"
+                alignOffset={-15}
+                sideOffset={-5}
+              >
+                <motion.div
+                  initial={{ opacity: 0, y: -5 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -5 }}
+                  transition={{ duration: 0.2, ease: "easeOut" }}
+                >
+                  <p className="text-sm">
+                    Sign in to get exclusive rewards & benefits <br />
+                    New Customer?{" "}
+                    <span className="underline font-bold">Create account</span>
+                  </p>
+                </motion.div>
+              </TooltipContent>
+            </Tooltip>
           </li>
+
+          {/* Shopping Icons */}
           <li className="cursor-pointer hover-lift">
-            <ShoppingBag className="w-5 h-6 cursor-pointer" strokeWidth={1.5} />
+            <ShoppingBag
+              className="w-5 h-6 cursor-pointer"
+              strokeWidth={1.5}
+              onClick={() => setAddToCart(true)}
+            />
           </li>
         </ul>
       </div>
@@ -100,27 +172,31 @@ function Main_Navbar({isScrolled}) {
         transition={{ type: "spring", stiffness: 300, damping: 30 }}
         className="overflow-hidden lg:hidden px-4"
       >
-
-      <div className="lg:hidden block pb-5 px-4">
-        <div
-          onClick={() => setShowSearch(true)}
-          className="rounded-4xl bg-gray-100 hover:bg-white border border-transparent hover:border hover:border-gray-300 hover:cursor-pointer py-3 px-4 flex gap-3"
-        >
-          <Search className="w-5 h-6 cursor-pointer" strokeWidth="1.5" />
-          <span className="text-gray-400 text-sm">
-            What are you looking for to...
-          </span>
+        <div className="lg:hidden block pb-5 px-4">
+          <div
+            onClick={() => setShowSearch(true)}
+            className="rounded-4xl bg-gray-100 hover:bg-white border border-transparent hover:border hover:border-gray-300 hover:cursor-pointer py-3 px-4 flex gap-3"
+          >
+            <Search className="w-5 h-6 cursor-pointer" strokeWidth="1.5" />
+            <span className="text-gray-400 text-sm">
+              What are you looking for to...
+            </span>
+          </div>
         </div>
-      </div>
-
-              
       </motion.div>
 
       {/* SearchBox Overlay */}
       <SearchBoxOverlay showSearch={showSearch} setShowSearch={setShowSearch} />
 
       {/* Hamburger_Menu Overlay */}
-      <HamburgerOverlay showMenu={showMenu} setShowMenu={setShowMenu} setShowSearch={setShowSearch}/>
+      <HamburgerOverlay
+        showMenu={showMenu}
+        setShowMenu={setShowMenu}
+        setShowSearch={setShowSearch}
+      />
+
+      {/* AddToCart Overlay */}
+      <AddToCartOverlay addToCart={addToCart} setAddToCart={setAddToCart} />
     </div>
   );
 }
