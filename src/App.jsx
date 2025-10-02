aae8b3a9fa58ef8fd67e9451f9678b1ad13c9d87
zeeshanwdev27 <zeeshanwdev27@gmail.com>
2025-10-02 18:01:13 +0500

    added loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import React from "react"
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom"
 import Layout from "./layout/Layout.jsx"
-import Home from "./pages/Home/Home.jsx"
-import Men_Section from "./pages/Men_Section/Men_Section.jsx"
-import Women_Section from "./pages/Women_Section/Women_Section.jsx"
-import Product from './pages/Product/Product.jsx'
+
+const Home = React.lazy(()=> import('./pages/Home/Home.jsx'))
+const Men_Section = React.lazy(()=> import('./pages/Men_Section/Men_Section.jsx'))
+const Women_Section = React.lazy(()=> import('./pages/Women_Section/Women_Section.jsx'))
+const Product = React.lazy(()=> import('./pages/Product/Product.jsx'))
+// import Home from "./pages/Home/Home.jsx"
+// import Men_Section from "./pages/Men_Section/Men_Section.jsx"
+// import Women_Section from "./pages/Women_Section/Women_Section.jsx"
+// import Product from './pages/Product/Product.jsx'
 
 
 const App = createBrowserRouter(
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,20 +1,32 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar.jsx";
 import Footer from "../components/Footer/Footer.jsx";
-
+import { Suspense } from "react";
 
 function Layout() {
   return (
     <>
-    <Navbar/>
+      <Navbar />
 
-    <div className='min-h-screen  pt-[120px]'>
-    <Outlet/>
-    </div>
-    
-     <Footer/>
+      <div className="min-h-screen  pt-[120px]">
+        <Suspense
+          fallback={
+            <div className="flex items-center justify-center py-70">
+              <div className="flex gap-2">
+                <span className="w-3 h-3 rounded-full bg-gray-800 animate-bounce [animation-delay:-0.3s]"></span>
+                <span className="w-3 h-3 rounded-full bg-gray-800 animate-bounce [animation-delay:-0.15s]"></span>
+                <span className="w-3 h-3 rounded-full bg-gray-800 animate-bounce"></span>
+              </div>
+            </div>
+          }
+        >
+          <Outlet />
+        </Suspense>
+      </div>
+
+      <Footer />
     </>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
