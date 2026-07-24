4251b9e27c7db9f9f0ff9cd004fda9a7d1d2a4ea
zeeshanwdev27 <zeeshanwdev27@gmail.com>
2026-07-24 19:21:46 +0500

    change product_cards

--- src/components/Products_Cards.jsx
+++ src/components/Products_Cards.jsx
@@ -47,17 +47,11 @@ function Products_Cards({ category, gender, page, setPage, setCardData, setCardA
           <div className="grid grid-cols-2 lg:grid-cols-4 gap-2 w-full overflow-hidden">
           {products.map((item) => (
             <Card key={item.productId} className="overflow-hidden rounded-none shadow-none border-none transition py-0 gap-2"
-              onClick={() => {
-                if (window.innerWidth < 1024) {
-                  setCardData(item);
-                  setCardActive(true);
-                }
-              }}
             >
               {/* Product Image */}
               <div className="relative w-full aspect-[3/3.7] overflow-hidden group">
 
-                <img src={item.image[0]} alt={`${item.productName}_front`} className="w-full h-full object-cover group-hover:opacity-0 transition duration-500 absolute top-0 left-0"/>
+                <img src={item.image[0]} alt={`${item.productName}_front`} className="w-full h-full object-cover group-hover:opacity-0 transition duration-500 absolute top-0 left-0" onClick={()=> navigate(`/collections/product/${item.productId}`)}/>
                 <img src={item.image[1]} alt={`${item.productName}_back`} className="hidden lg:block w-full h-full object-cover opacity-0 group-hover:opacity-100 transition duration-500 absolute top-0 left-0" onClick={()=> navigate(`/collections/product/${item.productId}`)}/>
 
                 {/* Size Button Overlay */}
@@ -93,7 +87,17 @@ function Products_Cards({ category, gender, page, setPage, setCardData, setCardA
               <CardContent className="flex justify-between px-0">
 
                 <div className="flex flex-col gap-0.3">
-                  <h2 className="text-md lg:text-sm font-semibold truncate">{item.productName}</h2>
+
+                  <h2 className="text-md lg:text-sm font-semibold truncate" 
+                  onClick={() => {
+                    if (window.innerWidth < 1024) {
+                      setCardData(item);
+                      setCardActive(true);
+                    }
+                    }}>
+                      {item.productName}
+                   </h2>
+                   
                   <p className="text-sm text-gray-500">{item.category}</p>
                   <p className="text-sm text-gray-500">{item.color}</p>
                   <p className="text-sm font-bold">${item.price}</p>
