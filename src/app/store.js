06b57e6afb31865d842d3d846ffd8d07f021aa82
zeeshanwdev27 <zeeshanwdev27@gmail.com>
2026-07-26 01:09:38 +0500

    fix store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "@/features/Cart/CartSlice";
-import productReducer from "@/features/product/productSlice";
+import productReducer from "@/features/Product/productSlice";
 import userReducer from "@/features/Auth/authSlice";
 
 export const store = configureStore({
